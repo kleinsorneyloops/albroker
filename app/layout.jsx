059784c4b3d55f1230ae183e31c02c402abaf194
kleinsorneyloops <klein.soren@gmail.com>
2026-03-27 15:02:19 -0600

    Update layout.jsx
    branding

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,8 +16,8 @@ export default function RootLayout({ children }) {
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
             </head>
-            <body className="antialiased text-white bg-deepspace">
-                <div className="flex flex-col min-h-screen px-6 bg-noise sm:px-12">
+            <body className="antialiased" style={{ backgroundColor: 'var(--bg)', color: 'var(--text)' }}>
+                <div className="flex flex-col min-h-screen px-6 sm:px-12">
                     <div className="flex flex-col w-full max-w-5xl mx-auto grow">
                         <Header />
                         <main className="grow">{children}</main>
